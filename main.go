b0e9b170463950ae51cef25651d803ded8c41222
Luke Gardener <luke.g@bitocean.com>
2019-11-28 15:17:00 +0900

    First commit

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+const AddForm = `
+<form method="POST" action="/add">
+URL: <input type="text" name="url">
+<input type="submit" value="Add">
+</form>
+`
+
+func homePage(writer http.ResponseWriter, r *http.Request) {
+	writeFileToHtml(writer, "html/index.html")
+	////fmt.Fprintf(writer, "Hello home page")
+	//
+	//html, err := ioutil.ReadFile("html/index.html")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//t := template.New("table")
+	//
+	////t, err := t.Parse(AddForm)
+	//t, err2 := t.Parse(string(html))
+	//if err2 != nil {
+	//	panic(err)
+	//}
+	//
+	//writer.Header().Set("Content-Type", "text/html; charset=utf-8")
+	////fmt.Fprint(w, AddForm)
+	//
+	//t.Execute(os.Stdout,nil)
+	//t.Execute(writer, nil)
+}
+
+func writeFileToHtml(writer http.ResponseWriter, filename string) {
+	html, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	t := template.New("page")
+
+	t, err2 := t.Parse(string(html))
+	if err2 != nil {
+		panic(err)
+	}
+
+	writer.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	t.Execute(os.Stdout,nil)
+	t.Execute(writer, nil)
+}
+
+func startServer() {
+	http.HandleFunc("/", homePage)
+
+	error := http.ListenAndServe(":9901", nil)
+	fmt.Println(error)
+}
+
+func main() {
+	startServer()
+}
